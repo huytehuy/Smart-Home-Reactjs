903a6cda391712589e6e33e1760dc730c9acc17f
huytehuy <phamkhanhhuy1231@gmai.com>
2023-05-06 15:08:16 +0700

    hello

--- src/pages/Faceid.js
+++ src/pages/Faceid.js
@@ -28,7 +28,7 @@ function Faceid() {
 
   // Update name in session storage when it changes
   useEffect(() => {
-    sessionStorage.setItem('username','huy');
+    sessionStorage.setItem('username',name);
   }, [name]);
   useEffect(() => {
     sessionStorage.setItem('mssv',mssv)
@@ -91,7 +91,7 @@ const faceSignIn = async () => {
     console.log('PayLoad: ', userData.payload)
     setName(decodeURIComponent(escape(window.atob(userData.payload.username))))
     setMssv(userData.payload.userId)
-    sessionStorage.setItem('username',name)
+    // sessionStorage.setItem('username',name)
     navigate('/login', { replace: true });
     // document.querySelector('.name').innerHTML += `<p>Mã số sinh viên: ${userData.payload.userId}`
     // document.querySelector('.name').innerHTML += `<p>Tên đầy đủ: ${decodeURIComponent(escape(window.atob(userData.payload.username)))}`
